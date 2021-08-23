d47a8438982150e067f5d83a24b1d72107f14fed
Stephanie <me@stephaniemariewatson.com>
2021-08-23 18:16:00 -0400

    Initial commit for Exercises 37 and 38

---
+++ Exercise_38/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] userArray = new int[5];
+            string userAnswer = string.Empty;
+
+            do
+            {
+                for (i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Enter a number: ");
+                    userArray[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int average = (userArray[0] + userArray[1] + userArray[2] + userArray[3] + userArray[4]) /5;
+
+                string averageTotal = ($"({userArray[0]} + {userArray[1]} + {userArray[2]} + {userArray[3]} + {userArray[4]})/5 = {average}");
+
+               
+                Console.WriteLine($"{averageTotal}");
+
+                Console.Write("Would you like to continue (y/n)? ");
+                userAnswer = Console.ReadLine().ToLower();
+
+                if (userAnswer == "n")
+                {
+                    Console.WriteLine("GoodBye");
+                }
+
+            } while (userAnswer == "y");
+
+        }
+
+    }
+}
---
+++ Exercises_37_38/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercises_37_38
+{ //Exercise 37
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userAnswer = string.Empty;
+            do
+            {
+                Console.WriteLine($"{ArraySum(new int[5])}");
+
+                Console.Write("Would you like to continue (y/n)? ");
+                userAnswer = Console.ReadLine().ToLower();
+
+                if (userAnswer == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+
+            } while (userAnswer == "y");
+
+
+        }
+
+
+        public static string ArraySum(int[] userArray)
+        {
+            int i;
+            userArray = new int[5];
+            string userAnswer = string.Empty;
+
+
+                for (i = 0; i < 5; i++)
+                {
+                    Console.Write("Enter a number: ");
+                    userArray[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int sum = userArray[0] + userArray[1] + userArray[2] + userArray[3] + userArray[4];
+
+                string sumTotal = ($"{userArray[0]} + {userArray[1]} + {userArray[2]} + {userArray[3]} + {userArray[4]} = {sum}");
+
+                return $"{sumTotal}";
+
+        }
+    }
+}
